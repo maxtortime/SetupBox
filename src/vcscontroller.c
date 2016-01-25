d1fe406ea344e316a1c1629a3df9e74f7e880611
Jaewon Choi <jainersoer@ajou.ac.kr>
2016-01-25 16:52:13 +0900

    dev: Make include dir and adjust header file loc

--- src/vcscontroller.c
+++ src/vcscontroller.c
@@ -14,3 +14,44 @@ static struct
 {
 }_sb_vcs_context;
 
+error_code_t sb_init(enum VCS vcs, const char* dir)
+{
+	if (vcs == VCS_GIT) {
+		git_libgit2_init();
+	}
+}
+
+error_code_t sb_destroy()
+{
+}
+
+// ASSUME: Currently, there exists "one" unique repo.
+error_code_t sb_update()
+{
+}
+
+error_code_t sb_commit()
+{
+}
+
+/*
+ * @author: Jaewon Choi, Jungmo Ahn
+ * @param: const char* target
+ * 	It's Okay whether target is a file or directory.
+ * @todo: please add a comment like this to all functions.
+ */
+error_code_t sb_add(const char* target)
+{
+}
+
+/*
+ * @author: Jaewon Choi, Jungmo Ahn
+ * @param: const char* target
+ * 	It's Okay whether target is a file or directory.
+ * @todo: please add a comment like this to all functions.
+ */
+error_code_t sb_remove(const char* target)
+{
+}
+
+
