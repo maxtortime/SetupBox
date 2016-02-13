ed4e967e72d1046ca1961ae6bfcd6235fd787e57
Taehwan Kim <maxtortime@gmail.com>
2016-02-13 22:31:19 +0900

    dev: Change vcscore to __init__.py
    Because python package recognize __init__.py

--- setupbox/__init__.py
+++ setupbox/__init__.py
@@ -1,2 +1,26 @@
-def welcome():
-    print ("Welcome!")
+# -*- coding: utf-8 -*-
+from abc import ABCMeta, abstractmethod
+
+class VcsCore:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def update(self):
+        pass
+
+    @abstractmethod
+    def add(self, filename):
+        pass
+
+    @abstractmethod
+    def commit(self, msg):
+        pass
+
+class VcsCoreSvn(object):
+    def __init__(self, repo_dir):
+        pass
+
+
+
+
+VcsCore.register(VcsCoreSvn)
