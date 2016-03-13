57edf989562671c8b39196fce6ec3630a61c2af2
Taehwan Kim <maxtortime@gmail.com>
2016-03-13 16:00:35 +0900

    Fix assert error and remove unused package

--- web_server/runserver.py
+++ web_server/runserver.py
@@ -208,7 +208,7 @@ def file_delete():
 
 
 if __name__ == '__main__':
-    assert len(sys.argv) > 2, "Usage: python runserver.py develop"
+    assert len(sys.argv) > 1, "Usage: python runserver.py develop"
 
     if sys.argv[1] == "develop":
         app.run(threaded=True)
